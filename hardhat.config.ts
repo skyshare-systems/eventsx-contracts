329e988a948724bfe5ee5a824cdfadb6d6e6c371
hvstle-dev <hvstle.dev@gmail.com>
2024-01-10 01:10:41 +0800

    chore: redeploy to mainnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,12 +14,17 @@ const config: HardhatUserConfig = {
       url: "https://bsc-dataseed.bnbchain.org",
       accounts: [process.env.WALLET_MAIN_PRIV_KEY ?? ""],
     },
+    mainnet: {
+      url: "https://eth-mainnet.g.alchemy.com/v2/EZeIyzkcnVq-TTdMMl6d0n-38JSynLwc",
+      accounts: [process.env.WALLET_MAIN_PRIV_KEY ?? ""],
+    },
   },
   etherscan: {
     apiKey: {
       sepolia: process.env.SEPOLIA_API_KEY ?? "",
       bscTestnet: process.env.BSC_TESTNET_API_KEY ?? "",
       bsc: process.env.BSC_TESTNET_API_KEY ?? "",
+      mainnet: process.env.SEPOLIA_API_KEY ?? "",
     },
   },
 };
